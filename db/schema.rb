83fe6f14a0848b63c9a4c349dbc20834ec80763b
Ronaldo <kikogamer@gmail.com>
2018-08-13 23:31:34 -0300

    adding link_hashtag migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_021339) do
+ActiveRecord::Schema.define(version: 2018_08_14_022528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2018_08_14_021339) do
   create_table "hashtags", force: :cascade do |t|
     t.string "name"
     t.integer "company_id"
+  end
+
+  create_table "link_hashtags", force: :cascade do |t|
+    t.integer "link_id"
+    t.integer "hashtag_id"
   end
 
   create_table "links", force: :cascade do |t|
